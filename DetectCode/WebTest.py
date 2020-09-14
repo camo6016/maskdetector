e9cc7a05129498125f3a598429928914a5a136d7
Cameron Mitchelll <cameron.mitchell54@gmail.com>
2020-09-14 15:20:09 +1000

    Update WebTest.py

--- DetectCode/WebTest.py
+++ DetectCode/WebTest.py
@@ -31,6 +31,8 @@ os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 from flask import Flask, Response
 import cv2
 
+
+Address        = "0.0.0.0"
 Camera         = None
 CameraRun      = True
 Frame          = None
@@ -41,82 +43,98 @@ GenRawJPEGThread = None
 GenDetectJPEGThread = None
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
-	# grab the dimensions of the frame and then construct a blob
-	# from it
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-		(104.0, 177.0, 123.0))
+        # grab the dimensions of the frame and then construct a blob
+        # from it
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+                (104.0, 177.0, 123.0))
 
-	# pass the blob through the network and obtain the face detections
-	faceNet.setInput(blob)
-	detections = faceNet.forward()
+        # pass the blob through the network and obtain the face detections
+        faceNet.setInput(blob)
+        detections = faceNet.forward()
 
-	# initialize our list of faces, their corresponding locations,
-	# and the list of predictions from our face mask network
-	faces = []
-	locs = []
-	preds = []
+        # initialize our list of faces, their corresponding locations,
+        # and the list of predictions from our face mask network
+        faces = []
+        locs = []
+        preds = []
 
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
-		confidence = detections[0, 0, i, 2]
+        # loop over the detections
+        for i in range(0, detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with
+                # the detection
+                confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > 0.5:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+                # filter out weak detections by ensuring the confidence is
+                # greater than the minimum confidence
+                if confidence > 0.5:
+                        # compute the (x, y)-coordinates of the bounding box for
+                        # the object
+                        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                        (startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+                        # ensure the bounding boxes fall within the dimensions of
+                        # the frame
+                        (startX, startY) = (max(0, startX), max(0, startY))
+                        (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
-			face = np.expand_dims(face, axis=0)
+                        # extract the face ROI, convert it from BGR to RGB channel
+                        # ordering, resize it to 224x224, and preprocess it
+                        face = frame[startY:endY, startX:endX]
+                        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                        face = cv2.resize(face, (224, 224))
+                        face = img_to_array(face)
+                        face = preprocess_input(face)
+                        face = np.expand_dims(face, axis=0)
 
-			# add the face and bounding boxes to their respective
-			# lists
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
+                        # add the face and bounding boxes to their respective
+                        # lists
+                        faces.append(face)
+                        locs.append((startX, startY, endX, endY))
 
-	# only make a predictions if at least one face was detected
-	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
-		preds = maskNet.predict(faces)
+        # only make a predictions if at least one face was detected
+        if len(faces) > 0:
+                # for faster inference we'll make batch predictions on *all*
+                # faces at the same time rather than one-by-one predictions
+                # in the above `for` loop
+                preds = maskNet.predict(faces)
 
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
-	return (locs, preds)
+        # return a 2-tuple of the face locations and their corresponding
+        # locations
+        return (locs, preds)
 
 
 def VideoCamera():
     FPS = 1/30
     FPS_MS = int(FPS * 1000)
-    
+
     global Frame
     global Camera
     global CameraRun
+    global Address
 
     while CameraRun:
         (status, Frame) = Camera.read()
         time.sleep(FPS)
-        
+
+        if status!=True:
+
+            print("Trying to reconnect to a camera at" + Address)
+
+            capture = cv2.VideoCapture("rtsp://" + Address + ":5554/camera", cv2.CAP_FFMPEG)
+            capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+            time.sleep(2)
+            if capture.isOpened():
+                Camera=capture
+
+
 
 def GenerateRawWebFrame():
     while True:
+        if str(type(Frame)) == "<class 'NoneType'>":
+            time.sleep(2)
+            continue
+
         ret, jpeg = cv2.imencode('.jpg', Frame)
         encodedImage = jpeg.tobytes()
         global WebRawFrame
@@ -125,6 +143,11 @@ def GenerateRawWebFrame():
 
 def GenerateDetectWebFrame():
     while True:
+
+        if str(type(Frame)) == "<class 'NoneType'>":
+            time.sleep(2)
+            continue
+
         frame = copy.copy(Frame)
 
         # detect faces in the frame and determine if they are wearing a
@@ -157,7 +180,7 @@ def GenerateDetectWebFrame():
         global WebDetectFrame
         WebDetectFrame = b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n'
 
-     
+
 def YealdRawFrame():
     while True:
         #print("Frame")
@@ -192,6 +215,9 @@ def SetCamera(address):
     global CameraThread
     global GenRawJPEGThread
     global GenDetectJPEGThread
+    global Address
+
+    Address = address
 
     CameraRun = False
 
@@ -208,8 +234,7 @@ def SetCamera(address):
             break
 
     if Camera == None:
-        print("Camera Not Accessable")
-        return False
+        return "Unable to add Camera " + address
 
     CameraRun = True
     CameraThread = Thread(target=VideoCamera, args=())
